3bf624f48b54b6072b6c4831964a4990bd241866
Deepak Bhadoriya <deepakkbhadoriya@gmail.com>
2024-04-05 08:14:28 +0530

    fix: Minor ui fix

--- src/components/AddLocation.jsx
+++ src/components/AddLocation.jsx
@@ -9,23 +9,23 @@ import appContext from '../context/appContext';
 
 const AddLocation = () => {
   const { selectedLocation, setSelectedLocation } = appContext()
-  const {handleAddLocation} = useMapLocations()
+  const { handleAddLocation } = useMapLocations()
 
-  const handleOnNameChange =(e)=>{
-    setSelectedLocation({...selectedLocation, name:e.target.value })
+  const handleOnNameChange = (e) => {
+    setSelectedLocation({ ...selectedLocation, name: e.target.value })
   }
 
   return (
-    <Box>  
-      <TextField id="outlined-basic" size='small' label="New Location Name" variant="outlined" value={selectedLocation.name} onChange={handleOnNameChange} sx={{width:"100%"}} />
-    <Box sx={{display:"flex", justifyContent:"flex-end", mb:1}}>  
-      <IconButton aria-label="delete" onClick={handleAddLocation} sx={{background:"white"}}>
-        <CheckCircleIcon color='success' />
-      </IconButton>
-      <IconButton aria-label="delete" onClick={()=>setSelectedLocation(null)} sx={{background:"white"}}> 
-        <CancelIcon color='disabled' />
-      </IconButton>
-    </Box>
+    <Box sx={{background:"white", p:1, pb:0, mb:1 }}>
+      <TextField id="outlined-basic" size='small' label="New Location Name" variant="outlined" value={selectedLocation.name} onChange={handleOnNameChange} sx={{ width: "100%" }} />
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <IconButton aria-label="delete" onClick={handleAddLocation} sx={{ background: "white" }}>
+          <CheckCircleIcon color='success' />
+        </IconButton>
+        <IconButton aria-label="delete" onClick={() => setSelectedLocation(null)} sx={{ background: "white" }}>
+          <CancelIcon color='disabled' />
+        </IconButton>
+      </Box>
     </Box>
   )
 }
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -23,7 +23,7 @@ const MapContainer = () => {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <ZoomControl position="bottomright" />
+        <ZoomControl position="topright" />
         {selectedLocation && <MapPin isAddIcon position={selectedLocation}></MapPin>}
         {locations.map((location) => (
           <MapPin key={location.id} position={location}>
